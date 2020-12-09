fbf6a45bbb4113a8e36d3c448edb1f3047028d33
flonta <road0901_0825@yahoo.co.jp>
2020-12-09 09:46:53 +0900

    接続メッセージの追加2

--- testGit/src/test/GitTest.java
+++ testGit/src/test/GitTest.java
@@ -4,6 +4,10 @@ public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("Hello GitHub!");
 		System.out.println("GitHubに接続成功!!");
+		int a = 50;
+		int b = 20;
+		System.out.println(a + "は" + b + "より大きいです。");
+
 	}
 
 }
